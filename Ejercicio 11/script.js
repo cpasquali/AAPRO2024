e5f185bf6a871f17c972d4f0d8f1343cd65b5426
Constantino Pasquali <163750560+cpasquali@users.noreply.github.com>
2024-05-16 23:36:34 -0300

    Add files via upload

---
+++ Ejercicio 11/script.js
@@ -0,0 +1,62 @@
+
+// PRIMERA PARTE
+
+/*  const helado = 20;
+let precio = 0;
+let precioFinal = 0;
+let topping = prompt("Ingresa un topping");
+
+if (topping === "Oreo") {
+    precio = 10;
+} else if (topping === "KitKat") {
+    precio = 15;
+} else if (topping === "Kinder") {
+    precio = 25;
+} else {
+    console.log("No tenemos ese topping");
+}
+
+precioFinal = helado + precio;
+
+console.log(`El helado cuesta: ${precioFinal}`);  */
+
+//SEGUNDA PARTE
+
+/*  const pedido = prompt("Eliga carne, pescado o verdura");
+switch(pedido){
+    case "carne":
+        console.log("Ofrecemos vino tinto");
+        break;
+    case "pescado":
+        console.log("Ofrecemos vino blanco");
+        break;
+    case "verdura":
+        console.log("Ofrecemos agua");
+        break;
+    default:
+        console.log("Eliga carne, pescado o verdura");
+        break;
+}  */
+
+// TERCERA PARTE
+
+/* let array = [1,2,3,4,5]
+console.log("PRIMEROS VALORES DEL ARRAY")
+for(let i = 0; i < array.length; i++){
+    console.log(array[i])
+}
+
+array.push(0)
+
+console.log("SEGUNDOS VALORES DEL ARRAY")
+for(let i = 0; i < array.length; i++){
+    console.log(array[i])
+} */
+
+// CUARTA PARTE
+
+let numero = 0;
+while(numero < 11){
+    console.log(`${numero} es mas chico que 11`);
+    numero += 1;
+}
