fa205c1a2fb4e231d70db5d4fb9958b5ab114c04
Constantino Pasquali <163750560+cpasquali@users.noreply.github.com>
2024-05-16 21:41:16 -0300

    Add files via upload

---
+++ Ejercicio 10/script.js
@@ -0,0 +1,11 @@
+const nombre = "Constantino";
+const apellido = "Pasquali";
+console.log(`${nombre} ${apellido}`);
+
+const numero1 = 24;
+const numero2 = 30;
+const suma = numero1 + numero2;
+console.log(suma)
+
+const sumaTotal = suma + 20;
+console.log(sumaTotal)
